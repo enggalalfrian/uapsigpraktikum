f1c0160884eace3b45412b4de448c00994a0b89f
Salsabillaayuni <Salsabillaayuni31@gmail.com>
2021-06-22 23:47:37 -0700

    Commit tampilan oleh salsa

---
+++ application/views/index.php
@@ -0,0 +1,30 @@
+
+
+  <!-- Content Wrapper. Contains page content -->
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+  
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+  <!-- Content Wrapper. Contains page content -->
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div id="mapid" style="width: 800px; height: 400px;"></div>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
